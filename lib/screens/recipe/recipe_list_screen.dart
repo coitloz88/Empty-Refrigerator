2fcb3dfd7280a453987edf581d378b36f1bdebdf
Hyemin Lee <cotloz88@gmail.com>
2023-11-20 21:32:03 +0900

    recipe list screen 구현

---
+++ lib/screens/recipe/recipe_list_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/common_widgets/app_text.dart';
+import 'package:grocery_app/helpers/column_with_seprator.dart';
+import 'package:grocery_app/models/recipe_item.dart';
+
+class RecipeListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            padding: EdgeInsets.only(left: 25),
+            child: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        title: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 25,
+          ),
+          child: AppText(
+            text: "가능한 레시피들",
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Column(
+                children: getChildrenWithSeperator(
+                  addToLastChild: false,
+                  widgets: recipeItemDemo.map((e) {
+                    return Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 25,
+                      ),
+                      width: double.maxFinite,
+                      child: getRecipeRowWidget(e.title),
+                    );
+                  }).toList(),
+                  seperator: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25,
+                    ),
+                    child: Divider(
+                      thickness: 1,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getRecipeRowWidget(String title) {
+    return Padding(
+        padding: EdgeInsets.all(8),
+        child: ListTile(leading: Icon(Icons.restaurant), title: Text(title)));
+  }
+}
