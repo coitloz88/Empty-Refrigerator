e937376e2b5d6e910a0230470ae73aeb4dca15a0
Hyemin Lee <cotloz88@gmail.com>
2023-11-23 02:54:30 +0900

    통신 완성 및 필요없는 파일 삭제

--- lib/providers/recipe_list_state.dart
+++ lib/providers/recipe_list_state.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/models/ingredient_item.dart';
 import 'package:grocery_app/models/recipe_item.dart';
 
 class RecipeListState with ChangeNotifier {
   List<RecipeItem> _items = [];
-  
+
   List<RecipeItem> get items => _items;
 
-  void addItems(List<RecipeItem> newItems) {
-    _items = newItems;
+  void addItems(List<dynamic> newItems) {
+    debugPrint(newItems.toString());
+    _items = newItems.map((e) {
+      List<IngredientItem> ingredients = e['ingredient']
+          .asMap()
+          .entries
+          .map<IngredientItem>((ingredient) => IngredientItem(
+              checked: true, id: ingredient.key, name: ingredient.value))
+          .toList();
+
+      return RecipeItem(
+          title: e['title'], ingredients: ingredients, url: e['url']);
+    }).toList();
     notifyListeners();
   }
 }
